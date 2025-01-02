27479b12895f336624aaf0bd2a2e1c34c8bc149d
전성은 <144914292+yeouDend@users.noreply.github.com>
2025-01-02 11:00:55 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+
+ 
+
+    double answer = static_cast<double>(num1) / num2 * 1000;
+
+    return answer;
+}
