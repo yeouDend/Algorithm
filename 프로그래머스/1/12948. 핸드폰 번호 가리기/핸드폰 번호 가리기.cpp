0fe20eceb8138b2177130795c71b4112a019b7f8
전성은 <qkskqk0924@gmail.com>
2025-03-05 10:12:38 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) 
+{
+    string answer = phone_number;
+    for(int i = 0; i < phone_number.size()-4; ++i)
+    {
+        answer[i] = '*';
+    }
+    return answer;
+}
